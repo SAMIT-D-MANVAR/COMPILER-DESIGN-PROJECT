01e3c532a6a5a46009e5266638180dfaa5414a5e
SAMIT-D-MANVAR <69346963+SAMIT-D-MANVAR@users.noreply.github.com>
2024-01-29 00:41:18 +0530

    Add files via upload

---
+++ lexer.py
@@ -0,0 +1,63 @@
+SYMBOLS = ['(',
+           ')',
+           ';',
+           ',',
+           ':',
+           '\'']
+
+KEYWORDS = {'i': ['int', 'void'],
+            'm': ['main'],
+            'b': ['begin'],
+            'e':['expr'],
+            'y': ['end'],
+            'z': ['End'],
+            'd': ['0','1'],
+            'o': ['+', '-', '=','==','<=','>='],
+            'n': ['\n'],
+            'f': ['for']}
+
+OPERATORS = ['+', '-', '=','==','<=','>=']
+line_count = 0
+
+
+def getIndex(word):
+    keys = list(KEYWORDS.keys())
+    values = list(KEYWORDS.values())
+    for value in values:
+        if word in value:
+            i = values.index(value)
+            return keys[i]
+
+
+def get_tokens(filename):
+    tokens = []
+    F = open(filename, "r")
+    for line in F:
+        for word in line.split():
+            # Check if it's an isolated keyword
+            token = getIndex(word)
+            if token in KEYWORDS:
+                tokens.append(token)
+            else:
+                # Check if it's a keyword followed by a symbol
+                buffer = []
+                for character in word:
+                    if character.isalnum():
+                        # Serves a string builder
+                        buffer.append(character)
+                        current_word = "".join(buffer)
+                        token = getIndex(current_word)
+                        if token in KEYWORDS:
+                            # A fully formed keyword has been detected
+                            tokens.append(token)
+                            buffer = []
+                    elif character in SYMBOLS or character in OPERATORS:
+                        if len(buffer) != 0:
+                            tokens.append('v')
+                            buffer = []
+                        # If it's a special operator
+                        if character in SYMBOLS:
+                            tokens.append(character)
+                        else:
+                            tokens.append("o")
+    return tokens
